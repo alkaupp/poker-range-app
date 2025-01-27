d9f960fb2fd23f0b54f68343e7ea8c2ade5ee46d
Aleksi Kauppila <aleksi.kauppila@gmail.com>
2025-01-27 20:58:14 +0200

    change title

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ const hands = [
   "A2o", "K2o", "Q2o", "J2o", "T2o", "92o", "82o", "72o", "62o", "52o", "42o", "32o", "22"
 ];
 function App() {
+	document.title = 'Poker hand visualizer'
   // State to store the selected hand
   const [selectedHand, setSelectedHand] = useState(null);
 
